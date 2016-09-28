cd641f53e9ebf70d135072f66c1ef5cd59ec8dc2
Justin <justincyork@gmail.com>
2016-09-28 15:50:53 +0000

    ...

--- deploy.sh
+++ deploy.sh
@@ -55,7 +55,7 @@ git config user.email "travis@travis-ci.org"
 git config user.name "Travis"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
+if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
@@ -70,7 +70,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
